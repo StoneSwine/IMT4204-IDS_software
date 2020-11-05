d701ecc75abd4955bbd1daca5ae0795a8efce2b1
magnus <StoneSwine@users.noreply.github.com>
2020-11-05 18:58:36 +0100

    cooode

--- 03_anomaly/corr-coef.py
+++ 03_anomaly/corr-coef.py
@@ -6,18 +6,30 @@ import numpy as np
 
 # CHANGEME:
 setval = [1, 2, 3, 4]
+col=str(input("X or Y in column: "))
 probtab = np.loadtxt(open("data/corr-coef-pt.csv", "rb"),
                      delimiter=",", dtype='float')
+col_v = row_v = ""
+
 
 print("The marginal probabilities of all the values of 𝑋")
-px = np.sum(probtab, axis=0)
-py = np.sum(probtab, axis=1)
-print(f"     {'    '.join([str(i) for i in setval])}     P(y)")
+if col.lower() == "x":
+  col_v = "y"
+  row_v = "x"
+  px = np.sum(probtab, axis=0)
+  py = np.sum(probtab, axis=1)
+else:
+  col_v = "x"
+  row_v = "y"
+  py = np.sum(probtab, axis=0)
+  px = np.sum(probtab, axis=1)
+
+print(f"     {'    '.join([str(i) for i in setval])}     P({col_v})")
 for rl, row in zip(setval, probtab):
     print('%s   [%s]' % (rl, ' '.join('%04s' % i for i in row)), end=" ")
     print(f"- {np.sum(row, axis=0):.2f}")
 
-print("p(x) " + ' '.join(f"{i:.2f}" for i in px))
+print(f"p({row_v}) " + ' '.join(f"{i:.2f}" for i in px))
 
 print("- " * 10)
 
@@ -29,16 +41,16 @@ print("\nmean and variance for the variable 𝑋:")
 m_x = sum([(x*y) for x, y in zip(setval, px)])
 print("𝜇_x=", end="")
 for x, y in zip(setval, px):
-    print(f"{x}*{y} + ", end="")
-print(f"={m_x}")
+    print(f"{x}*{y:.3f} + ", end="")
+print(f"={m_x:.3f}")
 
 s_xs = sum([((x-m_x)**2)*y for x, y in zip(setval, px)])
 print("𝜎_x^2=", end="")
 for x, y in zip(setval, px):
-    print(f"({x}-{m_x})^2 *{y} + ", end="")
+    print(f"({x}-{m_x})^2 *{y:.3f} + ", end="")
 print(" = \n\t",end="")
 for x, y in zip(setval, px):
-    print(f"{((x-m_x)**2):.3f} *{y} + ", end="")
+    print(f"{((x-m_x)**2):.3f} *{y:.3f} + ", end="")
 print(f"={s_xs:.3f}")
 
 
@@ -46,16 +58,16 @@ print("\nMean and variance for the variable 𝑌:")
 m_y = sum([(x*y) for x, y in zip(setval, py)])
 print("𝜇_y=", end="")
 for x, y in zip(setval, py):
-    print(f"{x}*{y} + ", end="")
-print(f"={m_y}")
+    print(f"{x}*{y:.3f} + ", end="")
+print(f"={m_y:.3f}")
 
 s_ys = sum([((x-m_y)**2)*y for x, y in zip(setval, py)])
 print("𝜎_y^2=", end="")
 for x, y in zip(setval, py):
-    print(f"({x}-{m_y})^2 *{y} + ", end="")
+    print(f"({x}-{m_y})^2 *{y:.3f} + ", end="")
 print(" = \n\t",end="")
 for x, y in zip(setval, py):
-    print(f"{((x-m_y)**2):.3f} *{y} +", end="")
+    print(f"{((x-m_y)**2):.3f} *{y:.3f} +", end="")
 print(f"={s_ys:.3f}")
 
 
